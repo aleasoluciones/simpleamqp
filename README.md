d23938c9036c5eed729de03636f8b498bb6b560d
Eduardo Ferro <eduardo.ferro.aldama@gmail.com>
2014-12-27 00:44:14 +0100

    Minimal integration test (only to test that all pieces are connected ok)

--- README.md
+++ README.md
@@ -5,4 +5,5 @@
 SimpleAMQP is a minimal wrapper around the excelent AMQP library [github.com/streadway/amqp](http://github.com/streadway/amqp)
 It provided a AMQP Consumer and a AMQP Publisher
 
-
+## Tests
+To run the integration tests, make sure you have RabbitMQ running on any host, export the environment variable AMQP_URL=amqp://host/ and run go test -tags integration. TravisCI will also run the integration tests.
---
+++ simpleamqp_test.go
@@ -0,0 +1,42 @@
+package simpleamqp_test
+
+// +build integration
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"testing"
+
+	. "github.com/aleasoluciones/simpleamqp"
+	"github.com/stretchr/testify/assert"
+)
+
+func amqpUrlFromEnv() string {
+	url := os.Getenv("AMQP_URL")
+	if url == "" {
+		url = "amqp://"
+	}
+	return url
+}
+
+func TestPublishAndReceiveTwoMessages(t *testing.T) {
+	t.Parallel()
+	amqpUrl := amqpUrlFromEnv()
+	amqpConsumer := NewAmqpConsumer(amqpUrl)
+	messages := amqpConsumer.Receive("events", []string{"routingkey1"}, "test_queue", 30*time.Second)
+	log.Println(amqpConsumer)
+
+	time.Sleep(2 * time.Second)
+
+	amqpPublisher := NewAmqpPublisher(amqpUrl)
+	amqpPublisher.Publish("events", "routingkey1", []byte("irrelevantBody1"))
+	amqpPublisher.Publish("events", "routingkey1", []byte("irrelevantBody2"))
+
+	message1 := <-messages
+	assert.Equal(t, message1.Body, "irrelevantBody1")
+	message2 := <-messages
+	assert.Equal(t, message2.Body, "irrelevantBody2")
+
+}
