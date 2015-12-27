f06fd3d18f7b96f58f2b3182eea712c2427bbe8b
Alberto Perez <aperez@alea-soluciones.com>
2015-12-27 11:08:08 +0100

    Update AMQPConsumer mock

--- mocks/AMQPConsumer.go
+++ mocks/AMQPConsumer.go
@@ -1,9 +1,7 @@
 package mocks
 
-import (
-	"github.com/aleasoluciones/simpleamqp"
-	"github.com/stretchr/testify/mock"
-)
+import "github.com/aleasoluciones/simpleamqp"
+import "github.com/stretchr/testify/mock"
 
 import "time"
 
@@ -11,7 +9,31 @@ type AMQPConsumer struct {
 	mock.Mock
 }
 
-func (m *AMQPConsumer) Receive(exchange string, routingKeys []string, queue string, options simpleamqp.QueueOptions, queueTimeout time.Duration) chan simpleamqp.AmqpMessage {
-	ret := m.Called(exchange, routingKeys, queue, queueTimeout)
-	return ret.Get(0).(chan simpleamqp.AmqpMessage)
+func (_m *AMQPConsumer) Receive(exchange string, routingKeys []string, queue string, queueOptions simpleamqp.QueueOptions, queueTimeout time.Duration) chan simpleamqp.AmqpMessage {
+	ret := _m.Called(exchange, routingKeys, queue, queueOptions, queueTimeout)
+
+	var r0 chan simpleamqp.AmqpMessage
+	if rf, ok := ret.Get(0).(func(string, []string, string, simpleamqp.QueueOptions, time.Duration) chan simpleamqp.AmqpMessage); ok {
+		r0 = rf(exchange, routingKeys, queue, queueOptions, queueTimeout)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(chan simpleamqp.AmqpMessage)
+		}
+	}
+
+	return r0
+}
+func (_m *AMQPConsumer) ReceiveWithoutTimeout(exchange string, routingKeys []string, queue string, queueOptions simpleamqp.QueueOptions) chan simpleamqp.AmqpMessage {
+	ret := _m.Called(exchange, routingKeys, queue, queueOptions)
+
+	var r0 chan simpleamqp.AmqpMessage
+	if rf, ok := ret.Get(0).(func(string, []string, string, simpleamqp.QueueOptions) chan simpleamqp.AmqpMessage); ok {
+		r0 = rf(exchange, routingKeys, queue, queueOptions)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(chan simpleamqp.AmqpMessage)
+		}
+	}
+
+	return r0
 }
